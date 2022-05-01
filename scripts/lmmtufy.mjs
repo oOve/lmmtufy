07e02cc6d3437aa4f9c0f06a789dc90a54b0ae20
o_Ove <ovla@norceresearch.no>
2022-05-01 04:57:07 +0200

    First version

---
+++ scripts/lmmtufy.mjs
@@ -0,0 +1,33 @@
+/*
+▓█████▄  ██▀███           ▒█████  
+▒██▀ ██▌▓██ ▒ ██▒        ▒██▒  ██▒
+░██   █▌▓██ ░▄█ ▒        ▒██░  ██▒
+░▓█▄   ▌▒██▀▀█▄          ▒██   ██░
+░▒████▓ ░██▓ ▒██▒ ██▓    ░ ████▓▒░
+ ▒▒▓  ▒ ░ ▒▓ ░▒▓░ ▒▓▒    ░ ▒░▒░▒░ 
+ ░ ▒  ▒   ░▒ ░ ▒░ ░▒       ░ ▒ ▒░ 
+ ░ ░  ░   ░░   ░  ░      ░ ░ ░ ▒  
+   ░       ░       ░         ░ ░  
+ ░                 ░              
+ */
+
+ const MOD_NAME = "lmmtufy";
+
+
+let lmmtufy_socket;
+Hooks.once("socketlib.ready", () => {
+  // socketlib is activated, lets register our function
+	lmmtufy_socket = socketlib.registerModule("lmmtufy");	
+	lmmtufy_socket.register("_runMacro", _runMacro);
+});
+
+function _runMacro(macro){
+    let m = game.macros.getName(macro);
+    m.execute();
+}
+
+export function runMacroAs(player, macro){
+  let playerId = game.users.getName(player).id;
+  lmmtufy_socket.executeAsUser("_runMacro", playerId, macro);
+}
+window.runMacroAs = runMacroAs;
