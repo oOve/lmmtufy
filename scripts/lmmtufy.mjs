ff51f6d8b62913333e214318b38c289a77575359
o_Ove <ovla@norceresearch.no>
2022-05-02 14:00:01 +0200

    Will now send arguments to macro, and return its value back to caller

--- scripts/lmmtufy.mjs
+++ scripts/lmmtufy.mjs
@@ -21,21 +21,24 @@ Hooks.once("socketlib.ready", () => {
 	lmmtufy_socket.register("_runMacro", _runMacro);
 });
 
-function _runMacro(macro){
+function _runMacro(macro, ...args){
     let m = game.macros.getName(macro);
-    let author_id = m.data.author;
+    let author_id = m?.data?.author;
+    if (author_id === undefined){
+      return "Error: Macro", macro, "not found on client";
+    }
     if (game.users.get(author_id)?.isGM){
-      m.execute();
+      return m.execute(args);
     }
     else{
       console.error("Player tried to execute non-gm macro on your client!")
     }
 }
 
-export function runMacroAs(player, macro){
+export function runMacroAs(player, macro, ...args){
   if (game.user.isGM){
     let playerId = game.users.getName(player).id;
-    lmmtufy_socket.executeAsUser("_runMacro", playerId, macro);
+    return lmmtufy_socket.executeAsUser("_runMacro", playerId, macro, args);
   }
 }
 window.runMacroAs = runMacroAs;
